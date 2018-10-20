3a44a7d47a2490a8836f85e6fcb3b8dc57fe1777
kristipati sai kiran reddy <kskr777@gmail.com>
2018-10-20 16:18:39 +0530

    remvoal of api commit

--- web_app/urls.py
+++ web_app/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import url
-from rest_framework.urlpatterns import format_suffix_patterns
 from web_app import views as web_app_views
 
 urlpatterns = [
     url(r'^$',web_app_views.home , name='web_app-home'),
-    url(r'^api/',web_app_views.employeeList.as_view()),
     ]
--- web_app/views.py
+++ web_app/views.py
@@ -1,23 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from . models import employees
-from . serializers import employeesSerializer
+# Create your views here.zer
 
-class employeeList(APIView):
 
-    def get(self,request):
-        employee1=employees.objects.all()
-        serializer=employeesSerializer(employee1,many=True)
-        return Response(serializer.data)
-
-    def push(self):
-        pass
 
 def home(request):
     return render(request,'web_app/home.html')
